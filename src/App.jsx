eb52d73556768770ffec1dc10a37d10329cabeb5
theanhprocode <101663080+theanhprocode@users.noreply.github.com>
2025-03-29 14:32:11 +0700

    dark-mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,27 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ToggleColorMode() {
+  const { mode, setMode } = useColorScheme()
+
+  const toggleColorMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <Button onClick={toggleColorMode}>
+      Chuyển sang chế độ {mode === 'light' ? 'tối' : 'sáng'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ToggleColorMode/>
       <div>theanh</div>
 
       <Typography variant='h4' color='text.secondary'>theanh1</Typography>
