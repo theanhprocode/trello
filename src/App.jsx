332f7dae842fa3f450adcdec25ed2c8747cbdc58
theanhprocode <101663080+theanhprocode@users.noreply.github.com>
2025-03-31 00:16:31 +0700

    test dark-mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,49 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
+// import { useMediaQuery } from '@mui/material'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode';
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="dark-light-mode"
+          value={mode}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value='light'>
+            <LightModeIcon/>Light
+          </MenuItem>
+          <MenuItem value='dark'>
+            Dark
+          </MenuItem>
+          <MenuItem value='system'>
+            System
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
 
 function ToggleColorMode() {
+
   const { mode, setMode } = useColorScheme()
 
   const toggleColorMode = () => {
@@ -22,6 +63,7 @@ function App() {
 
   return (
     <>
+      <ModeSelect/>
       <ToggleColorMode/>
       <div>theanh</div>
 
