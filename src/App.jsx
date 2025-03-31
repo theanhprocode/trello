9841ded3a0fbe63832f80755030eb2fc074f452c
theanhprocode <101663080+theanhprocode@users.noreply.github.com>
2025-04-01 00:53:56 +0700

    project-structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,11 @@
-import { useColorScheme } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest'
-import Container from '@mui/material/Container'
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme()
-  const handleChange = (event) => {
-    const selectedMode = event.target.value
-    setMode(selectedMode)
-  }
-
-  return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
-        <Select
-          labelId="label-select-dark-light-mode"
-          id="dark-light-mode"
-          value={mode}
-          label="Mode"
-          onChange={handleChange}
-        >
-          <MenuItem value='light'>
-            <Box sx={{ display: 'flex', alighItems: 'center', gap: 1 }}>
-              <LightModeIcon fontSize='small'/>Light
-            </Box>
-          </MenuItem>
-          <MenuItem value='dark'>
-            <Box sx={{ display: 'flex', alighItems: 'center', gap: 1 }}>
-              <DarkModeIcon fontSize='small'/>Dark
-            </Box>
-          </MenuItem>
-          <MenuItem value='system'>
-            <Box sx={{ display: 'flex', alighItems: 'center', gap: 1 }}>
-              <SettingsSuggestIcon fontSize='small'/>System
-            </Box>
-          </MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  )
-}
+import Board from './pages/Boards/_id'
 
 
 function App() {
-
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box sx={{
-        backgroundColor: 'primary.light',
-        width: '100%',
-        height: (theme) => theme.customStyles.heightSmall,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        <ModeSelect/>
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.dark',
-        width: '100%',
-        height: (theme) => theme.customStyles.heightLarge,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board Bar
-      </Box>
-      <Box sx={{
-        backgroundColor: 'primary.main',
-        width: '100%',
-        height: (theme) => `calc(100vh - ${theme.customStyles.heightLarge} - ${theme.customStyles.heightSmall})`,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board Content
-      </Box>
-    </Container>
+    <>
+      <Board/>
+    </>
   )
 }
 
