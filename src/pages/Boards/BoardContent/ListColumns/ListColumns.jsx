30407be906497c096133ce131c4bc9c126f32b27
theanhprocode <101663080+theanhprocode@users.noreply.github.com>
2025-06-04 23:17:21 +0700

    done-ui -click-add

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -3,8 +3,19 @@ import Column from './Column/Column'
 import Button from '@mui/material/Button'
 import AddToPhotosIcon from '@mui/icons-material/AddToPhotos'
 import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
+import { useState } from 'react'
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import SearchIcon from '@mui/icons-material/Search'
+import CloseIcon from '@mui/icons-material/Close'
+
 
 function ListColumns({ columns }) {
+  const [openNewColumnForm, setOpenNewColumnForm] = useState(false)
+  const toggleNewColumnForm = () => setOpenNewColumnForm(!openNewColumnForm)
+
+  // const [searchValue, setSearchValue] = useState('')
+
 
   return (
     <div>
@@ -18,17 +29,58 @@ function ListColumns({ columns }) {
 
           {columns?.map(column => <Column key={column._id} column={column} /> )}
 
-          <Box sx={{
-            minWidth: '200px',
-            maxWidth: '200px',
-            mx: 2,
-            borderRadius: '6px',
-            height: 'fit-content',
-            bgcolor: '#ffffff3d'
-          }}>
-            <Button sx={{ color: 'white', width: '100%', justifyContent: 'flex-start', pl: 2.5, py: 1 }} startIcon={<AddToPhotosIcon />} >Add new column</Button>
-          </Box>
+          {!openNewColumnForm
+            ? <Box onClick={toggleNewColumnForm} sx={{
+              minWidth: '250px',
+              maxWidth: '250px',
+              mx: 2,
+              borderRadius: '6px',
+              height: 'fit-content',
+              bgcolor: '#ffffff3d'
+            }}>
+              <Button sx={{ color: 'white', width: '100%', justifyContent: 'flex-start', pl: 2.5, py: 1 }} startIcon={<AddToPhotosIcon />} >Add new column</Button>
+            </Box>
+            : <Box sx={{
+              minWidth: '250px',
+              maxWidth: '250px',
+              mx: 2,
+              p: 1,
+              borderRadius: '6px',
+              height: 'fit-content',
+              bgcolor: '#ffffff3d',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 1
+            }}>
+              <TextField
+                label="Enter column name"
+                type="text"
+                size='small'
+                variant='outlined'
+                autoFocus
+                // value={searchValue}
+                // onChange={(e) => setSearchValue(e.target.value)}
+                sx={{
+                  '& label': { color: 'white' },
+                  '& input': { color: 'white' },
+                  '& label.Mui-focused': { color: 'white' },
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': { borderColor: 'white' },
+                    '&:hover fieldset': { borderColor: 'white' },
+                    '&.Mui-focused fieldset': { borderColor: 'white' }
+                  }
+                }}
+              />
+              <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+                <Button
+                  variant="contained" color="success" size='small'
+                  sx={{ boxShadow: 'none', border: '1px solid', borderColor: (theme) => theme.palette.success.main, '&:hover': { bgcolor: (theme) => theme.palette.success.main } }}
+                >Add column</Button>
 
+                <CloseIcon onClick={toggleNewColumnForm} fontSize='small' sx={{ color: 'white', cursor: 'pointer', '&:hover': { color: (theme) => theme.palette.warning.light } }} />
+              </Box>
+            </Box>
+          }
         </Box>
       </SortableContext>
     </div>
