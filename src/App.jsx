da851e8b1f3d98ecefecbf609b52e0189e20cb0f
Anh Thế <anhthe@Anhs-MacBook-Pro.local>
2025-08-09 22:31:00 +0700

    done-config-router-form-auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Board from '~/pages/Boards/_id'
-import NotFound from '~/pages/404/NotFound.jsx'
+import NotFound from '~/pages/404/NotFound'
+import Auth from '~/pages/Auth/Auth'
 
 function App() {
   return (
@@ -13,6 +14,10 @@ function App() {
       } />
 
       <Route path='/boards/:boardId' element={<Board />} />
+
+      {/* Authentication */}
+      <Route path='/login' element={<Auth />} />
+      <Route path='/register' element={<Auth />} />
 
       {/* 404 not found page */}
       <Route path='*' element={<NotFound />} />
